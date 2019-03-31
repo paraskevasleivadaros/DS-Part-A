524c5e700c4b9be900f29cbef202a0ef241e611c
paraskevasleivadaros <paraskevasleivadaros@gmail.com>
2019-03-31 21:55:55 +0300

    Updated broker, publisher and subscriber

--- src/broker.java
+++ src/broker.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 public class broker {
 
@@ -31,20 +33,65 @@ public class broker {
 		public void run() {
 			PrintStream out;
 			Scanner in;
+			ArrayList busIDs;
+			ArrayList brokerLines = new ArrayList();
 			try {
 				out = new PrintStream(socket.getOutputStream());
 				in = new Scanner(socket.getInputStream());
-
-				System.out.println(socket.getPort());
-				System.out.println(socket.getInetAddress());
-				System.out.println(in.nextInt());
-				System.out.println(in.nextInt());
 				
-				out.println(3);
-				out.println(4);
+				busIDs = readBusLines();
+				System.out.println(busIDs);
+				
+				String IPandPort = socket.getInetAddress().toString() + socket.getPort();
+				IPandPort = SHA1(IPandPort);
+				
+				for (int i = 0; i < busIDs.size(); i++) {
+					if (SHA1((String)busIDs.get(i)).compareTo(IPandPort) == -1) {
+						brokerLines.add((String)busIDs.get(i));
+					}
+				}
+				
+				System.out.println(brokerLines);
+				
+				/*System.out.println(busIDs);
+				System.out.println(in.nextLine());
+				System.out.println(in.nextLine());*/
 			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public ArrayList readBusLines() {
+		try{ 
+		    FileReader in = new FileReader("C:\\Users\\xristos\\Documents\\Eclipse Workspace\\DS_Part1\\src\\busLinesNew.txt");
+		    BufferedReader br = new BufferedReader(in);
+		    ArrayList busIDs = new ArrayList();
+
+		    String line;
+		    String lineCode;
+		    while ((line = br.readLine()) != null) {
+		    	lineCode = line.substring(line.indexOf(',')+1, line.lastIndexOf(','));
+		    	busIDs.add(lineCode);
+		    }	
+		    in.close();
+		    return busIDs;
+		} catch (IOException e) {
+			System.out.println("File Read Error");
+			return null;
+		}
+	}
+	
+	public String SHA1 (String s) throws NoSuchAlgorithmException {
+		MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+		byte[] result = mDigest.digest(s.getBytes());
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < result.length; i++) {
+			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
 	}
 }
--- src/publisher.java
+++ src/publisher.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class publisher {
 
@@ -14,33 +14,77 @@ public class publisher {
 		for (int i = 0; i < 2; i++) {
 			requestSocket= new Socket(InetAddress.getByName("192.168.1.140"), 1917);
 			
-			new myThread(requestSocket).start();
+			new myThread(requestSocket, i).start();
 		}
 	}
 	
 	private class myThread extends Thread {
 		Socket socket;
+		int num;
 		
-		public myThread(Socket socket) {
+		public myThread(Socket socket, int num) {
 			this.socket = socket;
+			this.num = num;
 		}
 		
 		public void run() {
 			PrintStream out;
 			Scanner in;
+			ArrayList busIDs;
 			try {
 				out = new PrintStream(socket.getOutputStream());
 				in = new Scanner(socket.getInputStream());
 				
-				for (int i = 0; i < 2; i++) {
-					out.println(i);
-				}
+				busIDs = readBusLines(num);
 				
-				System.out.println(in.nextInt());
-				System.out.println(in.nextInt());
+				for (int i = 0; i < 2; i++) {
+					out.println(busIDs);
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	public ArrayList readBusLines(int i) {
+		try{ 
+		    FileReader in = new FileReader("C:\\Users\\xristos\\Documents\\Eclipse Workspace\\DS_Part1\\src\\busLinesNew.txt");
+		    BufferedReader br = new BufferedReader(in);
+		    ArrayList busIDs = new ArrayList();
+
+		    String line;
+		    String lineCode;
+		    while ((line = br.readLine()) != null) {
+		    	lineCode = line.substring(0, line.indexOf(','));
+		    	if (i == 0) {
+		    		if (Integer.parseInt(lineCode) < 900) busIDs.add(lineCode);
+		    	} else {
+		    		if (Integer.parseInt(lineCode) >= 900) busIDs.add(lineCode);
+		    	}
+		    }	
+		    in.close();
+		    return busIDs;
+		} catch (IOException e) {
+			System.out.println("File Read Error");
+			return null;
+		}
+	}
+	
+	public void readBusPositions() {
+		try{ 
+			FileReader in = new FileReader("C:\\Users\\xristos\\Documents\\Eclipse Workspace\\DS_Part1\\src\\busPositionsNew.txt");
+			BufferedReader br = new BufferedReader(in);                 //reading coordinates
+
+			String line;
+			    
+			while ((line = br.readLine()) != null) {
+			    	
+					String[] tokens = line.split(",");
+			        System.out.println(tokens[3]+" " + tokens[4]);
+			} 
+			in.close();
+		} catch (IOException e) {
+			System.out.println("File Read Error");
 		}
 	}
 }
