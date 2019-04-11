6156f52b170953ce80012fd6ac85b47b8028053b
p3150090 <paraskevasleivadaros@gmail.com>
2019-04-11 15:42:39 +0300

    Update all sh files

--- src/pub2.sh
+++ src/pub2.sh
@@ -1,4 +1,3 @@
-cd C:\Users\paras\IdeaProjects\DS-Part-A\src
-cd C:\Users\p3150090\IdeaProjects\DS-Part-A\src
+cd C:/Users/p3150090/IdeaProjects/DS-Part-A/src
 javac publisher.java
 java publisher 2
